def6c90dc48d88c723667d144f46adf519707984
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-11-02 15:47:31 +0500

    cds

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const app = express()
 const port = process.env.PORT || 8000
 
 // app.use(express.urlencoded({extended: true})); // New
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
 
-// app.get("/", (req,res)=>{
-//     res.json("server start")
-// })
+app.get("/", (req,res)=>{
+    res.json("server start")
+})
 // Routes
 
 const router = require('./src/routes/report1')
