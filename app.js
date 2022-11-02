1b8c8f56eff7854f41e72c0f98dd14300b3f569f
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-11-02 16:30:54 +0500

    scaf

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
+var cors = require('cors');
 
 const app = express()
 
 const port = process.env.PORT || 8000
 
 // app.use(express.urlencoded({extended: true})); // New
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
 // app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req,res)=>{
     res.json("server start")
 })
+
+app.use(express.json())
+app.use(cors());
 // Routes
 
 const router = require('./src/routes/report1')
